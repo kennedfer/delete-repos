1aaf211c4a6a5f93c7322feeb461662331adf632
Kenned Ferreira <74821126+kennedfer@users.noreply.github.com>
2025-03-19 12:36:12 +0000

    feat: Corrige o componente que exibe os repositorios, agora está exibindo as informações corretamente.

--- src/components/RepositoriesTable/RepositoriesTable.tsx
+++ src/components/RepositoriesTable/RepositoriesTable.tsx
@@ -1,12 +1,61 @@
-export default function RepositoriesTable({ repositories, page, setPage }) {
+
+interface Repository  {
+  "id": number,
+  "name": string,
+  "createdAt": string,
+  "updatedAt": string,
+  "size": string,
+  "commits": number,
+  "branches": number,
+  "description": string
+}
+
+function TableHead() {
+  return <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Created At</th>
+      <th scope="col">Last Update</th>
+      <th scope="col">Size</th>
+      <th scope="col">Commits</th>
+      <th scope="col">Branches</th>
+    </tr>
+  </thead>
+}
+
+function TableBody({repositories}){
+  console.log(repositories)
+
+  return <tbody>
+    {
+      repositories.map((repository: Repository) => 
+        <tr>
+          <th scope="row"><input type="checkbox" /></th>
+          <td>{repository.name}</td>
+          <td>{repository.description}</td>
+          <td>{repository.createdAt}</td>
+          <td>{repository.updatedAt}</td>
+          <td>{repository.size}</td>
+          <td>{repository.commits}</td>
+          <td>{repository.branches}</td>
+        </tr>
+      )
+    }
+  </tbody>
+}
+
+export default function RepositoriesTable({ repositories }) {
   return (
     <div>
-      {repositories.map((repository) => (
-        <div key={repository.id}>{repository.name}</div>
-      ))}
+      <table col>
+        <TableHead/>
+        <TableBody repositories={repositories}/>
+      </table>
       <div>
-        <button onClick={() => setPage((prev) => prev - 1)}>prev</button>
-        <button onClick={() => setPage((prev) => prev + 1)}>next</button>
+        <button>prev</button>
+        <button>next</button>
       </div>
     </div>
   );
